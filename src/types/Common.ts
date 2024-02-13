195b3102cc7494778b2942afda3d0f4d224d1374
Vedant <vedant@gmail.com>
2024-02-13 17:49:21 +0530

    new commit

--- src/types/Common.ts
+++ src/types/Common.ts
@@ -10,7 +10,7 @@ export type Company = {
   headquarters: string;
   homepage: string;
   id: number;
-  logo_path: string;
+  logo_path: string; 
   name: string;
   origin_country: string;
   parent_company: null | object;
